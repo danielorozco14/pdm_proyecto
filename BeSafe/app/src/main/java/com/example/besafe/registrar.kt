4d20472a8c25e7e15194f49da3400aff3a06afab
danielorozco14 <00200617@uca.edu.sv>
2019-06-24 10:49:00 -0600

    Nuestra creacion de usuarios cholera funciona a medias :V

--- BeSafe/app/src/main/java/com/example/besafe/registrar.kt
+++ BeSafe/app/src/main/java/com/example/besafe/registrar.kt
@@ -11,32 +11,29 @@ import kotlinx.android.synthetic.main.activity_registrar.*
 class registrar : AppCompatActivity() {
 
     var mAuth = FirebaseAuth.getInstance()
-     var flag:Boolean=false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrar)
         registra()
-        if(flag)
-            Toast.makeText(this, "Registro Completado", Toast.LENGTH_LONG).show()
+
+
     }
 
     private fun registra() {
 
-        var mCorreoRegistro = "ceciSeLaCome@mbienDoblada.com"// correo_registro.text.toString()
-        var mContraRegistro = "PutoElQueLoLea"//contra_registro.text.toString()
-
         button_registro.setOnClickListener {
+            var mCorreoRegistro = correo_registro.text.toString()
+            var mContraRegistro = contra_registro.text.toString()
             mAuth.createUserWithEmailAndPassword(mCorreoRegistro, mContraRegistro).addOnCompleteListener(this) {
                 if (it.isSuccessful) {
                     startActivity(Intent(this, BeSafe::class.java))
+                    Toast.makeText(this, "Registro Completado", Toast.LENGTH_LONG).show()
                     Log.d("SIGN UP", "creacionUsuarioConCorreo: hecho")
-                    flag=true
                 } else {
                     Toast.makeText(this, "Error registrandose", Toast.LENGTH_LONG).show()
                 }
             }
         }
-
-
     }
 }

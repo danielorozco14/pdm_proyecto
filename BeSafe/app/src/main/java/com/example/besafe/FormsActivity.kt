a398e5d79402aa24431189a74ddf41062f0fbc3e
danielorozco14 <00200617@uca.edu.sv>
2019-06-24 21:49:53 -0600

    Arreglado error de librerias

--- BeSafe/app/src/main/java/com/example/besafe/FormsActivity.kt
+++ BeSafe/app/src/main/java/com/example/besafe/FormsActivity.kt
@@ -4,19 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.cv_form_list_item.*
 
 class FormsActivity : AppCompatActivity() {
 
+    val db = FirebaseFirestore.getInstance()
+    val TAG="/D"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forms)
-        val db = FirebaseFirestore.getInstance()
+
         val user = hashMapOf(
             "first" to "Ada",
             "last" to "Lovelace",
             "born" to 1815
         )
-        val TAG="/D"
+
         // Add a new document with a generated ID
         db.collection("users")
             .add(user)
@@ -27,10 +30,17 @@ class FormsActivity : AppCompatActivity() {
                 Log.w(TAG, "Error adding document", e)
             }
 
+
+    }
+
+    override fun onStart() {
+        super.onStart()
         db.collection("users")
             .get()
             .addOnSuccessListener { result ->
                 for (document in result) {
+
+                    text_Test.text = document.data.get("born").toString()
                     Log.d(TAG, "${document.id} => ${document.data}")
                 }
             }
---
+++ BeSafe/app/src/main/java/com/example/besafe/adapters/test_rvAdapter.kt
@@ -0,0 +1,8 @@
+package com.example.besafe.adapters
+
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+
+class test_rvAdapter {
+
+}
